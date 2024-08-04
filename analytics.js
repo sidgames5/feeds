7f7e4dff6509df1eec6a391ed01ef9a8756d8305
Sid <saytleader@outlook.com>
2024-08-04 08:47:14 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,16 +1,18 @@
-const userAgent = navigator.userAgent;
-const currentUrl = window.location.href;
+document.addEventListener('DOMContentLoaded', function() {
+    const userAgent = navigator.userAgent;
+    const currentUrl = window.location.href;
 
-fetch('https://analytics.thefossrant.com/collector', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        userAgent: userAgent,
-        url: currentUrl
+    fetch('https://analytics.thefossrant.com/collector', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            userAgent: userAgent,
+            url: currentUrl
+        })
     })
-})
     .then(response => response.json())
     .then(data => console.log('Success:', data))
     .catch((error) => console.error('Error:', error));
+});
